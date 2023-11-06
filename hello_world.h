fda2e916e32eb0b448cbce66774ac1a699dd296e
FrancisDavison <francis.nj.davison@gmail.com>
2023-11-06 18:29:04 +0000

    playing around, tried to make hello world, lots of random stuff appeared

---
+++ hello_world.h
@@ -0,0 +1,14 @@
+//
+// Created by Defiant on 06/11/2023.
+//
+
+#ifndef LEARNING_CPP_HELLO_WORLD_H
+#define LEARNING_CPP_HELLO_WORLD_H
+
+
+class hello_world {
+
+};
+
+
+#endif //LEARNING_CPP_HELLO_WORLD_H
