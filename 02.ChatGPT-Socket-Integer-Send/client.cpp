37f2620b46af0cd232ecefc5abd5dc08181f9c2a
FrancisDavison <francis.nj.davison@gmail.com>
2023-11-10 18:22:57 +0000

    added chatgpt generated code, will play when RPi's are set up

--- 02.ChatGPT-Socket-Integer-Send/client.cpp
+++ 02.ChatGPT-Socket-Integer-Send/client.cpp
@@ -1,3 +1,66 @@
 //
 // Created by Defiant on 10/11/2023.
 //
+
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+    // Create a socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Error creating socket\n";
+        return -1;
+    }
+
+    // Connect to the server
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("server_ip_address"); // Replace with the IP address of your server
+    serverAddress.sin_port = htons(12345); // Same port used in the server
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error connecting to server\n";
+        close(clientSocket);
+        return -1;
+    }
+
+    // Send the start message
+    const char startMessage[] = "10101";
+    if (send(clientSocket, startMessage, sizeof(startMessage), 0) == -1) {
+        std::cerr << "Error sending start message\n";
+        close(clientSocket);
+        return -1;
+    }
+
+    std::cout << "Communication started!\n";
+
+    while (true) {
+        // Send integer to the server
+        int valueToSend;
+        std::cout << "Enter an integer to send: ";
+        std::cin >> valueToSend;
+
+        if (send(clientSocket, &valueToSend, sizeof(valueToSend), 0) == -1) {
+            std::cerr << "Error sending data\n";
+            break;
+        }
+
+        // Receive integer from the server
+        int receivedValue;
+        if (recv(clientSocket, &receivedValue, sizeof(receivedValue), 0) == -1) {
+            std::cerr << "Error receiving data\n";
+            break;
+        }
+
+        std::cout << "Received value: " << receivedValue << std::endl;
+    }
+
+    // Close the socket
+    close(clientSocket);
+
+    return 0;
+}
--- 02.ChatGPT-Socket-Integer-Send/server.cpp
+++ 02.ChatGPT-Socket-Integer-Send/server.cpp
@@ -1,3 +1,92 @@
 //
 // Created by Defiant on 10/11/2023.
 //
+
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+    // Create a socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Error creating socket\n";
+        return -1;
+    }
+
+    // Bind the socket to a specific port
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(12345); // You can use any available port
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error binding socket\n";
+        close(serverSocket);
+        return -1;
+    }
+
+    // Listen for incoming connections
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Error listening for connections\n";
+        close(serverSocket);
+        return -1;
+    }
+
+    std::cout << "Server is waiting for a connection...\n";
+
+    // Accept a connection
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket == -1) {
+        std::cerr << "Error accepting connection\n";
+        close(serverSocket);
+        return -1;
+    }
+
+    // Receive the start message
+    char startMessage[6];
+    if (recv(clientSocket, startMessage, sizeof(startMessage), 0) == -1) {
+        std::cerr << "Error receiving start message\n";
+        close(clientSocket);
+        close(serverSocket);
+        return -1;
+    }
+
+    if (std::string(startMessage) != "10101") {
+        std::cerr << "Invalid start message\n";
+        close(clientSocket);
+        close(serverSocket);
+        return -1;
+    }
+
+    std::cout << "Communication started!\n";
+
+    while (true) {
+        // Receive integer from the client
+        int receivedValue;
+        if (recv(clientSocket, &receivedValue, sizeof(receivedValue), 0) == -1) {
+            std::cerr << "Error receiving data\n";
+            break;
+        }
+
+        std::cout << "Received value: " << receivedValue << std::endl;
+
+        // Send integer to the client
+        int valueToSend;
+        std::cout << "Enter an integer to send: ";
+        std::cin >> valueToSend;
+
+        if (send(clientSocket, &valueToSend, sizeof(valueToSend), 0) == -1) {
+            std::cerr << "Error sending data\n";
+            break;
+        }
+    }
+
+    // Close sockets
+    close(clientSocket);
+    close(serverSocket);
+
+    return 0;
+}
